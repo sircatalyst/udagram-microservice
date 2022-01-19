cadfc535b04a1ed1d73796e36024ae5a9ad082bf
Temitope Bamidele <temibami@gmail.com>
2022-01-19 23:05:04 +0100

    fix error with k8s

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a353a377d09bc40d881322b96cc65cbe-661826439.us-east-2.elb.amazonaws.com/api/v0'
+  // apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
